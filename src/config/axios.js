399d7c5d88d30b73410cd5dcd591d0f0266f7b1a
Bharat Shukla <bharatsagii30@gmail.com>
2024-03-09 23:26:59 +0530

    https change

--- src/config/axios.js
+++ src/config/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
     //Uncomment the local host for local testing purposes
     //baseURL: "http://localhost:5000",
-    baseURL: "http://52.71.254.31:5000",
+    baseURL: "https://neighborly.in",
     withCredentials: true
 });
 

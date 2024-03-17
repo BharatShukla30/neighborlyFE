774461b1e3b790a48c52ccbba16a114a0dcfeb16
Bharat Kumar Shukla <bharatsagii30@gmail.com>
2024-03-17 20:57:59 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import store from "./redux/store"
 
 import './App.css'
 import Header from './components/Header';
-import Home from './pages/home';
+import Home from './pages/Home';
 import Footer from './components/Footer';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';

69bece820d3f962d20a3cb8e10f0abecee863b0b
Bharat Kumar Shukla <bharatsagii30@gmail.com>
2024-03-17 21:07:08 +0530

    Update axios.js

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "https://neighborly.in/", 
+    baseURL: "https://neighborly.in/api", 
     withCredentials: true
 });
 

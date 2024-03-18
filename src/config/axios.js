b12ae9d786609407f7dde184cad5c0d16c076cbb
Parmesh Kumar <parmeshkumar414@gmail.com>
2024-03-18 11:31:53 +0530

    feat: Chat socket is integrated with backend for real time chatting

--- src/config/axios.js
+++ src/config/axios.js
@@ -2,8 +2,9 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     //Uncomment the local host for local testing purposes
-    //baseURL: "http://localhost:5000",
-    baseURL: "http://52.71.254.31:5000",
+    // baseURL: "http://localhost:5000",
+    // baseURL: "http://52.71.254.31:5000",
+    baseURL: import.meta.env.VITE_REACT_APP_API_URL,
     withCredentials: true
 });
 
--- src/redux/actions/chatActions.js
+++ src/redux/actions/chatActions.js
@@ -17,9 +17,9 @@ export const getUserChats = createAsyncThunk(
 
 export const getChatMessages = createAsyncThunk(
     "chat/messages",
-    async (chatId, {rejectWithValue}) => {
+    async ({chatId, page}, {rejectWithValue}) => {
         try {
-            const request = await axiosInstance.get(`/group/fetch-group-messages/${chatId}?page=1&limit=5`);
+            const request = await axiosInstance.get(`/group/fetch-group-messages/${chatId}?page=${page}&limit=30`);
             const response = await request.data;
             return response;
         }

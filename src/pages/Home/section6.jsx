ba67a28dd33a52e095cf1580142c13e035c0f4d1
devnirajk <86728508+devnirajk@users.noreply.github.com>
2024-09-11 00:44:20 +0530

    changed the anonymously

--- src/pages/Home/section6.jsx
+++ src/pages/Home/section6.jsx
@@ -1,8 +1,13 @@
-import image6 from '../../assets/Frame6.png'
+import image6 from '../../assets/Frame6.svg'
 const Section6 = () => {
     return (
         <section>
-            <img src={image6} alt="image6" />
+            <div className='flex bg-custom-blue p-10 items-center justify-center'>
+            <div className='bg-white rounded-full p-3 mr-6'>
+            <img className="h-44" src={image6} alt="incognito" />
+            </div>
+            <div className='text-6xl text-white font-semibold'>Do It All Anonymously!!!</div>
+            </div>
         </section>
     );
 }
